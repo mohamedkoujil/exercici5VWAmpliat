b4caf11ed800adbd6c420242bb6d7884503a5d08
MohamedKoujil <mohamed.koujil.7e7@itb.cat>
2023-12-15 10:01:40 +0100

    funciones calculPerdua y preuFinal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
     println("Hello World!")
+    calculPerdua ()
+    preuFinal (preu, calculPerdua())
+}
+
+fun calculPerdua (kilometros: Int): Double {
+    var perdua:Double = 0.0
+    for (i in 1..kilometros) {
+        perdua += 0.00001
+    }
+    return perdua
+}
+
+fun preuFinal(preu, perdua) {
+    return preu - (preu * (perdua / 100))
 }
