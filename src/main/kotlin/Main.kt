c1353435b3fdbef168dd8b32cc7c24945dc97589
MohamedKoujil <mohamed.koujil.7e7@itb.cat>
2023-12-15 10:26:24 +0100

    funciones calculPerdua y preuFinal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.math.round
 
 fun readFloat(pMessage : String, pMin : Int, pMax : Int) : Float{
     var valor = 0.0f
@@ -25,6 +26,22 @@ fun readFloat(pMessage : String, pMin : Int, pMax : Int) : Float{
     }while(!correctType)
     return valor
 }
+fun calculPerdua (kilometros: Float): Double {
+    var perdua:Double = 0.0
+    for (i in 1..kilometros.toInt()) {
+        perdua += 0.00001
+    }
+    return perdua
+}
+
+fun preuFinal(preu: Int, perdua: Double): Double {
+    var preuCamper = 0
+    if (checkCamper()) preuCamper = preu+20000
+    return round(preu - (preu * (perdua / 100)))
+}
 fun main() {
     var kilometros=readFloat("¿Cuantos kilometros tiene tu furgoneta?", 0, 10000000)
+    var preu = 73490
+    calculPerdua (kilometros)
+    println("preu de venta = ${preuFinal(preu, calculPerdua(kilometros))}€")
 }
