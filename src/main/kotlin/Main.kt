5bba0c85d4cf3ca24316df9661fa38dfbebe2bcb
annacano0 <anna.elisabet5@gmail.com>
2023-12-15 09:56:32 +0100

    commit 2: validacion de datos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
+import java.util.*
+
+fun readFloat(pMessage : String, pMin : Int, pMax : Int) : Float{
+    var valor = 0.0f
+    val scan = Scanner(System.`in`)
+    var correctType = false
+
+    do{
+        println(pMessage)
+        correctType = scan.hasNextFloat()
+
+        if(!correctType){
+            println("ERROR")
+        }else{
+            valor = scan.nextFloat()
+
+            if(valor < pMin || valor > pMax){
+                println("ERROR")
+                correctType = false
+            }
+        }
+
+        scan.nextLine()
+
+    }while(!correctType)
+    return valor
+}
 fun main() {
-    println("Hello World!")
+    var kilometros=readFloat("¿Cuantos kilometros tiene tu furgoneta?", 0, 10000000)
 }
