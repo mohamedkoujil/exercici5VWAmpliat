a793adedcae61b415572d44fddbedf53fe9d74c1
MohamedKoujil <mohamed.koujil.7e7@itb.cat>
2023-12-18 12:48:07 +0100

    patch

--- src/main/kotlin/FuncionsVW.kt
+++ src/main/kotlin/FuncionsVW.kt
@@ -1,18 +1,18 @@
 fun preuBase(coche: Int): Float {
-    var preu = 73490.0F
+    var preu    = 73490.0F
     if (coche == 2) preu += 20000
     return preu
 }
-fun calculPerdua (kilometres: Int): Int {
-    var perdua = 0
-    when {
-        kilometres < 5000 -> perdua = 0
-        kilometres < 10000 -> perdua = 200
-        kilometres >= 10000 -> perdua = 300
+fun calculPerdua (kilometres: Double): Double {
+    val perdua = when {
+        kilometres < 5000 -> 0.0
+        kilometres < 10000 -> 200.0
+        kilometres >= 10000 -> 300.0
+        else -> 0.0
     }
-    return perdua
+    return perdua + kilometres*0.00001
 }
 
-fun preuFinal(preu: Float, perdua: Int): Float {
-    return preu - perdua
+fun preuFinal(preu: Float, perdua: Double): Float {
+    return preu - perdua.toFloat()
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,9 @@
-
 fun main() {
     val coche = readInt("Seleccioni el seu vehicle\n" +
             "1. Volkswagen Grand California\n" +
             "2. Volkswagen Grand California Camper Full Equip", 1, 2)
 
-    val km = readInt("Kilometratje dels pneumatics", 0, 10000000)
+    val km = readDouble("Kilometratje: ", 0, 100000000)
 
     println("Preu Basse: ${preuBase(coche)}€\nPreu Final: ${preuFinal(preuBase(coche), calculPerdua(km))}€")
 }
-//
