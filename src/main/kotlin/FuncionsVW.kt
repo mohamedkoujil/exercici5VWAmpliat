1cb8a69373b19a81512e8b7ad2fa9af443a4d60c
mohakjl <mohamedkoujil75@gmail.com>
2023-12-22 21:08:09 +0100

    Added Dokka documentation

--- src/main/kotlin/FuncionsVW.kt
+++ src/main/kotlin/FuncionsVW.kt
@@ -1,11 +1,54 @@
 import kotlin.math.round
 
+/**
+ * This method displays a welcome message in the console
+ * @since 22/12/2023
+ * @autor Mohamed Koujil
+ * @version 1.0.0
+ */
+fun benvinguda() {
+    println(BLUE_BOLD +
+            "   ---------------------------.\n" +
+            " `/\"\"\"\"/\"\"\"\"/|\"\"|'|\"\"||\"\"|   ' \\.\n" +
+            " /    /    / |__| |__||__|      |\n" +
+            "/----------=====================|\n" +
+            "| \\  /V\\  /    _.               |\n" +
+            "|()\\ \\W/ /()   _            _   |\n" +
+            "|   \\   /     / \\          / \\  |-( )\n" +
+            "=C========C==_| ) |--------| ) _/==] _-{_}_)\n" +
+            " \\_\\_/__..  \\_\\_/_ \\_\\_/ \\_\\_/__.__.\n" +
+            RESET)
+    val benvinguda = "Benvingut a la calculadora de preu de vehicles Volkswagen Grand California".split(" ")
+    val colors = arrayOf(YELLOW_UNDERLINED, GREEN_UNDERLINED, BLUE_UNDERLINED, PURPLE_UNDERLINED, CYAN_UNDERLINED, WHITE_UNDERLINED)
+    var cont = -1
+    for (i in benvinguda.indices) {
+
+        if (cont == colors.indices.last) cont = 0
+        else cont++
+
+        print(colors[cont] + "${benvinguda[i]} " + RESET)
+    }
+    println()
+}
+
+/**
+ * This method can be used to calculate the base price according to the type of vehicle
+ * @param coche Type of vehicle
+ * @return preu price of the vehicle
+ */
 fun preuBase(coche: Int): Float {
     var preu = 73490.0F
     if (coche == 2) preu += 20000
     return preu
 }
 
+/**
+ * This method can be used to calculate the loss of value according to the kilometers, years and if it has a bike rack
+ * @param kilometres Kilometers of the vehicle
+ * @param anys Years of the vehicle
+ * @param portaBicis If the vehicle has a bike rack
+ * @return perdua Loss of value of the vehicle
+ */
 fun calculPerdua (kilometres: Double, anys: Int, portaBicis: Boolean): Float {
 
     var perdua: Double = when {
@@ -28,6 +71,12 @@ fun calculPerdua (kilometres: Double, anys: Int, portaBicis: Boolean): Float {
     return (round(perdua * 100) / 100).toFloat()
 }
 
+/**
+ * This method can be used to calculate the final price according to the base price and the loss of value
+ * @param preu Base price of the vehicle
+ * @param perdua Loss of value of the vehicle
+ * @return preuFinal Final price of the vehicle
+ */
 fun preuFinal(preu: Float, perdua: Float): Float {
     return preu - perdua
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,6 @@
 fun main() {
-    println(BLUE_BACKGROUND + BLACK_BOLD_BRIGHT +
-            "   ---------------------------.\n" +
-            " `/\"\"\"\"/\"\"\"\"/|\"\"|'|\"\"||\"\"|   ' \\.\n" +
-            " /    /    / |__| |__||__|      |\n" +
-            "/----------=====================|\n" +
-            "| \\  /V\\  /    _.               |\n" +
-            "|()\\ \\W/ /()   _            _   |\n" +
-            "|   \\   /     / \\          / \\  |-( )\n" +
-            "=C========C==_| ) |--------| ) _/==] _-{_}_)\n" +
-            " \\_\\_/__..  \\_\\_/_ \\_\\_/ \\_\\_/__.__.\n" +
-            RESET)
-    val benvinguda = "Benvingut a la calculadora de preu de vehicles Volkswagen Grand California".split(" ")
-    val colors = arrayOf(YELLOW_UNDERLINED, GREEN_UNDERLINED, BLUE_UNDERLINED, PURPLE_UNDERLINED, CYAN_UNDERLINED, WHITE_UNDERLINED)
-    var cont = -1
-    for (i in benvinguda.indices) {
 
-        if (cont == colors.indices.last) cont = 0
-        else cont++
-
-        print(colors[cont] + "${benvinguda[i]} " + RESET)
-    }
-    println()
+    benvinguda()
 
     val coche = readInt("Seleccioni el seu vehicle\n" +
                         "1. Volkswagen Grand California\n" +
